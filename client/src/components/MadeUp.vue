e3f4721808eb319710619858c8233a21027711ac
‘e <1191231182@qq.com>
2020-03-25 19:52:34 +0800

    更新左下和左上视图

--- client/src/components/MadeUp.vue
+++ client/src/components/MadeUp.vue
@@ -9,47 +9,47 @@
 let echarts = require("echarts");
 export default {
   name: "madeup",
-  mounted() {
-    var data = [
-      { value: 75, name: "男性" },
-      { value: 25, name: "女性" }
-    ];
-    var dataBar = [10, 52, 200, 334, 390, 330, 220];
-    console.log(data);
-    this.drawpie(data);
-    this.drawbar(dataBar);
+  data() {
+    return {
+        sexData: null,
+        ageData: null,
+        myChart: null,
+        option: null
+      } ;
   },
-
+    mounted() {},
   methods: {
-    drawpie(data) {
-      var myChart = echarts.init(document.getElementById("in-pie"));
-      var option = {
+    drawpie() {
+        let that=this;
+      that.myChart = echarts.init(document.getElementById("in-pie"));
+      that.option = {
         title: {
           text: "性别占比",
-          top: "10px"
+          top: "25px",
+          left: "15%"
         },
         tooltip: {
-          trigger: "item"
+          trigger: "item",
         },
         series: [
           {
             name: "感染者性别",
             type: "pie",
-            center: ["24%", "70%"],
+            center: ["28%", "74%"],
             radius: "50%",
-            data: data
+            data: that.sexData,
           }
         ]
       };
-      myChart.setOption(option);
+      that.myChart.setOption(that.option,true);
     },
-    drawbar(dataBar) {
-      var myChart = echarts.init(document.getElementById("in-bar"));
-      var option = {
+    drawbar() {
+        let that=this;
+      that.myChart = echarts.init(document.getElementById("in-bar"));
+      that.option = {
         color: ["#3398DB"],
         title: {
           text: "年龄组成",
-
           left: "65%"
         },
         tooltip: {
@@ -70,15 +70,7 @@ export default {
           {
             type: "category",
             name: "年龄段",
-            data: [
-              "10-20",
-              "20-30",
-              "30-40",
-              "40-50",
-              "50-60",
-              "60-70",
-              "70-80"
-            ],
+            data: ["0-10","10-20","20-30","30-40","40-50","50-60","60-70","70-80"],
             axisTick: {
               alignWithLabel: true
             }
@@ -94,23 +86,54 @@ export default {
             name: "患病人数",
             type: "bar",
             barWidth: "30%",
-            data: dataBar
+            data: that.ageData,
           }
         ]
       };
-      myChart.setOption(option);
+      that.myChart.setOption(that.option);
     }
   },
   computed: {
     madeupdata() {
       return this.$store.getters.getmadeupdata;
-    }
+    },
+    ScTrackData() {
+      return this.$store.getters.getscTrackData;
+    },
   },
   watch: {
-    //监听dailydata数据变化
+    //监听madeupdata数据变化
     madeupdata: function(newval, oldval) {
       //图表数据变化后该执行的操作
-    }
+      this.sexData=newval.sex;
+      this.ageData=newval.age;
+      this.drawpie();
+      this.drawbar();
+    },
+      ScTrackData: function (newval, oldval) {
+        var madeupdata={
+          sex: [{name:'', value: 0},{name: '', value: 0}],
+          age: [],
+        };
+        var j=0;
+        for(var i=0;i<newval.length;i++)
+        {
+            if(newval[i].sex=="男")
+            {
+                madeupdata.sex[0].value+=1;
+                madeupdata.sex[0].name="男";
+            }
+            else if(newval[i].sex=="女")
+            {
+                madeupdata.sex[1].value+=1;
+                madeupdata.sex[1].name="女";
+            }
+            madeupdata.age[j]=newval[i].age;
+            j++;
+        }
+        console.log(madeupdata);
+          this.$store.commit("setmadeupdata", madeupdata);
+      }
   }
 };
 </script>
