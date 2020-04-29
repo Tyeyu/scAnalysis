bb408e1ce24ec65429a40cd52c3628dadab2634a
Tyeyu <905109256@qq.com>
2020-04-29 23:18:34 +0800

    关联地区图绘制

--- client/src/components/testCalendar.vue
+++ client/src/components/testCalendar.vue
@@ -1,13 +1,51 @@
 <template>
-  <div id="testCalendar"></div>
+  <div id="testCalendar">
+    <div>
+      <span>关联地区风险</span>
+    </div>
+  </div>
 </template>
 <script>
 import * as d3 from "d3";
 export default {
   data() {
     return {
-      incity: [1],
-      outprovinces: [1],
+      incity: [
+        { name: "成都", value: 0 },
+        { name: "自贡", value: 0 },
+        { name: "攀枝花", value: 0 },
+        { name: "泸州", value: 0 },
+        { name: "德阳", value: 0 },
+        { name: "绵阳", value: 0 },
+        { name: "广元", value: 0 },
+        { name: "遂宁", value: 0 },
+        { name: "内江", value: 0 },
+        { name: "乐山", value: 0 },
+        { name: "南充", value: 0 },
+        { name: "宜宾", value: 0 },
+        { name: "广安", value: 0 },
+        { name: "达州", value: 0 },
+        { name: "巴中", value: 0 },
+        { name: "雅安", value: 0 },
+        { name: "眉山", value: 0 },
+        { name: "资阳", value: 0 },
+        { name: "阿坝", value: 0 },
+        { name: "甘孜", value: 0 },
+        { name: "凉山", value: 0 }
+      ],
+      outprovinces: [
+        { cityname: "重庆", value: 0 },
+        { cityname: "云南", value: 0 },
+        { cityname: "陕西", value: 0 },
+        { cityname: "贵州", value: 0 },
+        { cityname: "宁夏", value: 0 },
+        { cityname: "西藏", value: 0 },
+        { cityname: "青海", value: 0 },
+        { cityname: "陕西", value: 0 },
+        { cityname: "甘肃", value: 0 },
+        { cityname: "湖南", value: 0 },
+        { cityname: "湖北", value: 0 }
+      ],
       svgwidth: null,
       svgheight: null,
       tabechange: false
@@ -23,23 +61,39 @@ export default {
         .append("svg")
         .attr("width", this.svgwidth)
         .attr("height", this.svgheight);
-      svg
-        .append("g")
+      var height = this.svgwidth / 100;
+      console.log(height);
+      var incityg = svg.append("g");
+      incityg
         .selectAll("bar")
         .data(this.incity)
         .enter()
         .append("rect")
-        .attr("width", 100)
-        .attr("height", 100)
+        .attr("x", function(d, i) {
+          return height * 2 + (i % 11) * height * 8 + (i % 11) * height;
+        })
+        .attr("y", function(d, i) {
+          if (i >= 11) {
+            return 18 * height;
+          } else {
+            return 8 * height;
+          }
+        })
+        .attr("width", height * 8)
+        .attr("height", height * 8)
         .attr("fill", "red");
-      svg
-        .append("g")
+      var outpg = svg.append("g");
+      outpg
         .selectAll("rect")
         .data(this.outprovinces)
         .enter()
         .append("rect")
-        .attr("width", 20)
-        .attr("height", 20)
+        .attr("y", 30 * height)
+        .attr("x", function(d, i) {
+          return height * 2 + i * height * 8 + i * height;
+        })
+        .attr("width", height * 8)
+        .attr("height", height * 8)
         .attr("fill", "green");
     }
   },
@@ -59,6 +113,10 @@ export default {
 };
 </script>
 <style>
+#testCalendar span {
+  color: white;
+  font: 18px "Microsoft YaHei";
+}
 #testCalendar {
   position: absolute;
   top: 65.1%;
